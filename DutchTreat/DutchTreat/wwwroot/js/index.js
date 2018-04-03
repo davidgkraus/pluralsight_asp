47a21045812eb26dcd3a3df2d9046e57c41b8e04
unknown <david.gustav.kraus@gmail.com>
2018-04-03 19:32:55 +0200

    uploaded repository

---
+++ DutchTreat/DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,14 @@
+﻿
+console.log("Moshi Mosh!");
+
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = false;
+
+var button = document.getElementById("BuyButton");
+button.addEventListener("click", function () {
+    console.log("Buying item");
+});
+
+var productInfo = document.getElementById("product-props");
+var listItems = productInfo.item[0].children;
